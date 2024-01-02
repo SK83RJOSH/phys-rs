4bf76db6d3dd91bafe92ee1d87477be6e1f34f7e
SK83RJOSH <sk83rjosh@gmail.com>
2024-01-02 10:29:46 +0100

    feat(float2): add fmt + from support

--- src/math/float2.rs
+++ src/math/float2.rs
@@ -1,3 +1,5 @@
+#[cfg(not(spirv))]
+use core::fmt;
 use core::{f32, ops::*};
 
 /// Creates a 2-dimensional vector.
@@ -42,5 +44,50 @@ impl IndexMut<usize> for Float2 {
             1 => &mut self.y,
             _ => panic!("index out of bounds"),
         }
+    }
+}
+
+#[cfg(not(spirv))]
+impl fmt::Display for Float2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}, {}]", self.x, self.y)
+    }
+}
+
+#[cfg(not(spirv))]
+impl fmt::Debug for Float2 {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_tuple(stringify!(Float2))
+            .field(&self.x)
+            .field(&self.y)
+            .finish()
+    }
+}
+
+impl From<[f32; 2]> for Float2 {
+    #[inline]
+    fn from(a: [f32; 2]) -> Self {
+        Self::new(a[0], a[1])
+    }
+}
+
+impl From<Float2> for [f32; 2] {
+    #[inline]
+    fn from(v: Float2) -> Self {
+        [v.x, v.y]
+    }
+}
+
+impl From<(f32, f32)> for Float2 {
+    #[inline]
+    fn from(t: (f32, f32)) -> Self {
+        Self::new(t.0, t.1)
+    }
+}
+
+impl From<Float2> for (f32, f32) {
+    #[inline]
+    fn from(v: Float2) -> Self {
+        (v.x, v.y)
     }
 }
